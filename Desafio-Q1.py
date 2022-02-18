1f485c6c6f8c50742793b9990e31a57484ae13c3
Luisa Santana Neves <luisa.santana.neves@gmail.com>
2022-02-18 20:15:45 -0300

    Questão 1
    Completando a questão 1

---
+++ Desafio-Q1.py
@@ -0,0 +1,11 @@
+def triangulo(n):
+
+    for i in range(n):
+        for j in range(1, n - i):
+            print(" ", end="")
+        for k in range(0, i + 1):
+            print("*", end="")
+        print()
+
+n = int(input('Digite um número: '))
+triangulo(n)
